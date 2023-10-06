40035055a01cbcdf91f882016ac74ec3d852021b
iarleisouza <iarlei.souza@gmail.com>
2023-10-05 22:03:21 -0300

    Subindo projeto

---
+++ index.js
@@ -0,0 +1,27 @@
+function calcula(tipo, valor) {
+
+  if (tipo === 'acao') {
+
+      if (valor === 'c') {
+        document.getElementById('resultado').value = ''
+      }
+  
+      if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+        document.getElementById('resultado').value += valor
+      }
+     
+       if (valor === '='){
+        let valorCampo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = valorCampo
+       }
+
+
+
+  }else if (tipo === 'valor') {
+
+    document.getElementById('resultado').value += valor
+
+  }
+
+
+}
